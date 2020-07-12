5b846a2aa1e444b6a4f91e1d865501c265a066ba
JohnEssam235 <56760033+JohnEssam235@users.noreply.github.com>
2020-07-12 15:38:31 +0200

    Create i2c.c

---
+++ swift_act project/i2c.c
@@ -0,0 +1,30 @@
+#include "i2c.h"
+void EEPROM_init(void)
+{
+  I2C1_Init(100000);         // initialize I2C communication
+}
+
+void EEPROM_writeByte(uint8 my_data,uint8 add)
+{
+
+  I2C1_Start();              // issue I2C start signal
+  I2C1_Wr(0xA2);            // send byte via I2C  (device address + W)
+  I2C1_Wr(add);                // send byte (address of EEPROM location)
+  I2C1_Wr(my_data);            // send data (data to be written)
+  I2C1_Stop();               // issue I2C stop signal
+}
+
+uint8 EEPROM_readByte(uint8 add)
+{
+    uint8 my_data;
+    I2C1_Start();              // issue I2C start signal
+  I2C1_Wr(0xA2);           // send byte via I2C  (device address + W)
+  I2C1_Wr(add);               // send byte (data address)
+
+  I2C1_Repeated_Start();     // issue I2C signal repeated start
+  I2C1_Wr(0xA3);             // send byte (device address + R)
+
+  my_data = I2C1_Rd(0u);       // Read the data (NO acknowledge)
+  I2C1_Stop();               // issue I2C stop signal
+  return my_data;
+}
